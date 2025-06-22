4d3ad1e9de4a348c7bde6b928ad7afd8fca1ad9b
KiriTrip <eggor01042010@gmail.com>
2025-06-22 22:07:58 +0300

    Made open app

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
-int main()
+static void app_activate(GApplication *app, gpointer *user_data)
 {
-    printf("Sosa Muzik Player!");
+    GtkWidget *window = gtk_application_window_new(GTK_APPLICATION (app));
+    gtk_window_set_title(GTK_WINDOW (window) ,"Muzik Player");
+    gtk_window_set_default_size(GTK_WINDOW (window), 525, 700);
+    gtk_window_present(GTK_WINDOW (window));
+}
 
-    return 0;
+int main(int argc, char **argv)
+{
+    GtkApplication *app = gtk_application_new("com.muzik.Player", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate) ,NULL);
+    int status = g_application_run(G_APPLICATION (app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
